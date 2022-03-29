1c69374137def7bca3c0dc2be410bc7aaadd6533
Zura88 <agungyudisaputro@gmail.com>
2022-03-29 16:51:51 +0700

    coba-bot

---
+++ listsell/genshin.js
@@ -0,0 +1,22 @@
+module.exports.genshin =
+`[ *GENSHIN IMPACT* ] ✅
+VIA LOGIN - LEGAL 100%
+🛒 OPEN 
+PROSES : 1 - 10 MENIT (SESUAI ANTRIAN)
+
+💎 60     Crystals = Rp. 11.700
+💎 300   Crystals = Rp. 58.500
+💎 980   Crystals = Rp. 175.500
+💎 1980 Crystals = Rp. 351.000
+💎 3280 Crystals = Rp. 585.000
+💎 6480 Crystals = Rp. 1,170.000
+
+Blessing = Rp. 58.500
+Gnostic Hym = Rp. 117.000
+Gnostic Chorus = Rp. 234,000
+
+• Legal 100℅
+• Dapat Invoice Google Play
+• Fast Proses
+
+*RATE BERUBAH-UBAH, MOHON CHAT ADMIN SEBELUM MELAKUKAN PEMBELIAN*`
